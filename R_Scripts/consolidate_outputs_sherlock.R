3f0856af15a187813a50a855e1b36c9bad31bff9
Patricia Levi <pajalevi@gmail.com>
2019-06-27 16:49:26 -0700

    more analysis

--- R_Scripts/consolidate_outputs_sherlock.R
+++ R_Scripts/consolidate_outputs_sherlock.R
@@ -26,15 +26,32 @@ source(paste0(baseFol,"/code/R_Scripts/consolidatedAnalysisFns.R"))
 
 runID = "base_noDRfullyear" #"hour1" #"avail2"
 runDate = "2019-05-02" 
-overlaplength = 24
+overlaplength = 6
 
 outputID = paste0(runID,"_",runDate)
 output_fol = paste0(baseFol,outputFolBase,outputID,"/")
 
 # consolidate and save info
-demanddual = loadTimeseriesData(output_fol,"supplydemand_shadow", overlaplength,1)
+
+print("Loading fast production data")
+fastprod = loadTimeseriesData(output_fol, "fast_production", overlaplength,2,instance_in_fol,params$nrandp,dist_ID = params$stochID, probabilities = F)
+print("Loading slow production data")
+slowprod = loadTimeseriesData(output_fol, "slow_production", overlaplength,2,instance_in_fol,params$nrandp,dist_ID = params$stochID, probabilities = F)
+slowprod$speed = "slow"
+fastprod$speed = "fast"
+prod = rbind(slowprod, fastprod)
+names(prod)[names(prod) == 'value'] <- 'MWout'
+rm(fastprod, slowprod)
+# write_csv(prod, paste0(output_fol,"prod.csv")) # cannot turn off scientific notation in write_csv
+prod$prob = 1/25
+write.csv(prod, paste0(output_fol,"prod.csv"))
+rm(prod)
+
+print("Loading demand dual")
+demanddual = loadTimeseriesData(output_fol,"supplydemand_shadow", overlaplength,2)
 write.csv(demanddual, paste0(output_fol,"demanddual.csv"))
 rm(demanddual) #memory cleanup
 
-mingendual = loadTimeseriesData(output_fol,"mingen_shadow", overlaplength,1) ## DOESNT WORK!!
+print("Loading mingen dual data")
+mingendual = loadTimeseriesData(output_fol,"mingen_shadow", overlaplength,2) ## DOESNT WORK!!
 write.csv(mingendual, paste0(output_fol,"mingendual.csv"))
